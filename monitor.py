6e6e8ba7ee0c06bf834636cd0c18f2b4cbf286b4
Sara Bee <sarabobo@gmail.com>
2020-04-24 06:48:37 +0000

    Add support for monstera's humidity sensor

--- monitor.py
+++ monitor.py
@@ -5,26 +5,54 @@ from statistics import mean
 from twitter import Twitter
 from thoughts import Thoughts
 
-lower_bound = 250
-threshold_reading = 425 # arbitrary
+moisture_threshold = 415 # arbitrary
+humidity_threshold = 28.0 # arbitrary
 unearthed_reading = 550 # if the sensor is not in the soil
 
 # Run this thing with --notweet to output the tweet to stdout
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--notweet', default=False, action='store_true')
+    parser.add_argument('--topic', default="moisture", action="store")
     return parser.parse_args()
 
-def get_reading(reading):
-    try:
-        return int(reading)
-    except:
-        return 0
+def parse_sample(sample_list):
+    readings = {
+        "temp": [],
+        "humidity": [],
+        "moisture": []
+        }
+
+    for reading in sample_list:
+        if '\r' not in reading:
+            break
+        reading = reading.strip()
+        reading = reading.split(':')
+
+        if len(reading) == 2:
+            key = reading[0]
+            val = reading[1]
+            if key == "moisture":
+                try:
+                    readings["moisture"].append(int(val))
+                except:
+                    print("we got a weird value or something")
+            elif key in ["humidity", "temp"]:
+                try:
+                    readings[key].append(float(val))
+                except:
+                    print("we got a weird value or something")
+    return readings
+
+def output(args, msg):
+    if (args.notweet):
+        print(msg)
+    else:
+        Twitter.tweet(msg)
 
 args = get_args()
-
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-sample = ser.read(100) # pull 100 bytes off serial
+sample = ser.read(200) # pull 200 bytes off serial
 
 try:
     decoded_sample = sample.decode("utf-8")
@@ -33,30 +61,27 @@ except:
     sys.exit()
 
 # turn the decoded sample string into a list of samples
-sample_list = decoded_sample.split('\r\n')
+sample_list = decoded_sample.split('\n')
 
-readings_list = []
-for reading in sample_list:
-    reading = get_reading(reading)
-    # sometimes a reading gets truncated; any value above lower_bound is real
-    if reading > lower_bound:
-        readings_list.append(reading)
+readings_list = parse_sample(sample_list)
 
-if len(readings_list):
-    avg_reading = int(round(mean(readings_list)))
+if args.topic == "moisture" and len(readings_list["moisture"]):
+    avg_reading = int(round(mean(readings_list["moisture"])))
     if avg_reading > unearthed_reading:
         # The sensor reading is high enough that it's probably not in soil
         msg = Thoughts.get_unearthed_thought(avg_reading)
-    elif avg_reading > threshold_reading:
+    elif avg_reading > moisture_threshold:
         # The reading is above the arbitrary dryness threshold
         msg = Thoughts.get_thirsty_thought(avg_reading)
     else:
         # nicenice
         msg = Thoughts.get_happy_thought(avg_reading)
 
-    if (args.notweet):
-        print(msg)
-    else:
-        Twitter.tweet(msg)
-else:
-    print("no readings")
+    output(args, msg)
+
+if args.topic == "humidity" and len(readings_list["humidity"]):
+    avg_reading = round(mean(readings_list["humidity"]), 1)
+
+    if avg_reading < humidity_threshold:
+        msg = Thoughts.get_dry_thought(avg_reading)
+        output(args, msg)
--- thoughts.py
+++ thoughts.py
@@ -1,6 +1,11 @@
 import random
+pages = [
+    "Um, @SaraBee? ",
+    "Hey! @SaraBee! ",
+    "Paging @SaraBee: "
+]
 
-thought_openers = [
+moisture_openers = [
     "My soil moisure reading is {} and ",
     "My sensor currently reads {} and ",
     "Moisture levels are at {} and "
@@ -11,7 +16,6 @@ happy_thoughts = [
     "I'm p comfy 😌",
     "I'm a happy camper 🛶🏕",
     "that's the way I like it 💦",
-    "@SaraBee is doing a great job 💯"
 ]
 
 thirsty_thoughts = [
@@ -21,16 +25,35 @@ thirsty_thoughts = [
     "I'm hecking thirsty!!"
 ]
 
+dry_openers = [
+    "I could use a misting! ",
+    "Would you mind turning on the humidifier? ",
+    "Could you get the spray bottle please? ",
+    "Turn the humidifier on, please! "
+]
+
+dry_thoughts = [
+    "My humidity is only {}% 🥵",
+    "{}% humidity is less than ideal 😒",
+    "I'm a tropical plant and like my humidity higher than {}% 😩",
+]
+
 class Thoughts:
 
     def get_happy_thought(reading):
-        opener = random.choice(thought_openers).format(str(reading))
+        opener = random.choice(moisture_openers).format(str(reading))
         return opener + random.choice(happy_thoughts)
 
     def get_thirsty_thought(reading):
-        opener = random.choice(thought_openers).format(str(reading))
-        return opener + random.choice(thirsty_thoughts)
+        page = random.choice(pages)
+        opener = random.choice(moisture_openers).format(str(reading))
+        return page + opener + random.choice(thirsty_thoughts)
 
     def get_unearthed_thought(reading):
-        opener = random.choice(thought_openers).format(str(reading))
+        opener = random.choice(moisture_openers).format(str(reading))
         return opener + "@SaraBee must have pulled out my sensor 🤷🏻‍♀️"
+
+    def get_dry_thought(reading):
+        page = random.choice(pages)
+        opener = random.choice(dry_openers)
+        return page + opener + random.choice(dry_thoughts).format(str(reading))
